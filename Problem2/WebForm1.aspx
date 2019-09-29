8750d8fcae6521aff5e6c4df6421f66834da2870
dell <dell@DELL-PC>
2019-09-29 19:52:24 +0300

    Add project files.

---
+++ Problem2/WebForm1.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Problem2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ Problem2/WebService1.asmx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.IO;
+
+namespace Problem2
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+        public string ValidateMethod()
+        {
+            string source = "C://Users//dell//Desktop//Input//";
+            string error = "C://Users//dell//Desktop//ErrorFile//";
+            string filename = "valid.xml";
+            string filename2 = "invalid.xml";
+
+            if (File.Exists(Path.Combine(source, filename)))
+            {
+                string destination = System.IO.Path.Combine(source, filename);
+                System.IO.File.Delete(destination);
+                return true + " 200 OK";
+
+            }
+            else
+            {
+                string destination = System.IO.Path.Combine(source, filename2);
+                string errordestination = System.IO.Path.Combine(error, filename2);
+
+                System.IO.File.Move(destination, errordestination);
+                return false + "500 status(internal server error).";
+
+            }
+
+
+        }
+    }
+   
+
+}
